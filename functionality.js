ea0ca5d3b952a8717f3e99a55700467a4c120217
ip1010 <ilijapandev1010@gmail.com>
2024-11-24 19:53:43 +0100

    fourth commit

---
+++ functionality.js
@@ -0,0 +1,31 @@
+let paintings = document.querySelector('.paintings')
+let Tip = paintings.querySelector('.Tip')
+let Swing = paintings.querySelector('.Swing')
+let MonaLisa = paintings.querySelector('.MonaLisa')
+let GirlWithPearl = paintings.querySelector('.GirlWithPearl')
+let StarryNight = paintings.querySelector('.StarryNight')
+let Scream = paintings.querySelector('.Scream')
+let GreatWave = paintings.querySelector('.GreatWave')
+let Back = paintings.querySelector('.Back')
+
+Tip.addEventListener('click', function(){
+    window.open("https://www.google.com/search?sca_esv=444e26e92038851a&sxsrf=ADLYWIIzRT8IjOFNtgsjyS8qtIZgNAFStQ:1732471502857&q=historical+art+work&udm=2&fbs=AEQNm0B8d6pWs88_vU-iCKxgHzr_ZvSuUFOEg-6Tli5xwXR4l9Cn_zr5gLXI3h-otrgC_rcnp3u62UlgmHHgnCJ17aTxercN6ffdDEvUeyWGcLoZVNQDNF5vky2oDJF1pYvLoVtKxYLTtzIYMr9vIAbQh7UKfYl2n41ILYNteEmaXtmMRo_KRuNXeXIkJBC7Q8yiq-x9VZhT-cCzfmQVmr0oR_mAZJyQVA&sa=X&ved=2ahUKEwjGh4ukx_WJAxWpVPEDHUqUGQYQtKgLegQIGBAB&biw=1920&bih=953&dpr=1");
+})
+MonaLisa.addEventListener('click', function(){
+    window.open("https://en.wikipedia.org/wiki/Mona_Lisa");
+})
+Swing.addEventListener('click', function(){
+    window.open("https://en.wikipedia.org/wiki/The_Swing_(Fragonard)");
+})
+GirlWithPearl.addEventListener('click', function(){
+    window.open("https://en.wikipedia.org/wiki/Girl_with_a_Pearl_Earring");
+})
+StarryNight.addEventListener('click', function(){
+    window.open("https://en.wikipedia.org/wiki/The_Starry_Night");
+})
+Scream.addEventListener('click', function(){
+    window.open("https://en.wikipedia.org/wiki/The_Scream");
+})
+GreatWave.addEventListener('click', function(){
+    window.open("https://en.wikipedia.org/wiki/The_Great_Wave_off_Kanagawa");
+})
