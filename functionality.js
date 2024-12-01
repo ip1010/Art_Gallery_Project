cb3190a2d7de4771b2b9c181476dba8c645ef5d4
ip1010 <ilijapandev1010@gmail.com>
2024-12-01 19:13:41 +0100

    Ver_1.0_Commit

--- functionality.js
+++ functionality.js
@@ -12,20 +12,20 @@ Tip.addEventListener('click', function(){
     window.open("https://www.google.com/search?sca_esv=444e26e92038851a&sxsrf=ADLYWIIzRT8IjOFNtgsjyS8qtIZgNAFStQ:1732471502857&q=historical+art+work&udm=2&fbs=AEQNm0B8d6pWs88_vU-iCKxgHzr_ZvSuUFOEg-6Tli5xwXR4l9Cn_zr5gLXI3h-otrgC_rcnp3u62UlgmHHgnCJ17aTxercN6ffdDEvUeyWGcLoZVNQDNF5vky2oDJF1pYvLoVtKxYLTtzIYMr9vIAbQh7UKfYl2n41ILYNteEmaXtmMRo_KRuNXeXIkJBC7Q8yiq-x9VZhT-cCzfmQVmr0oR_mAZJyQVA&sa=X&ved=2ahUKEwjGh4ukx_WJAxWpVPEDHUqUGQYQtKgLegQIGBAB&biw=1920&bih=953&dpr=1");
 })
 MonaLisa.addEventListener('click', function(){
-    window.open("https://en.wikipedia.org/wiki/Mona_Lisa");
+    alert("The Mona Lisa is a half-length portrait painting by Italian artist Leonardo da Vinci. Considered an archetypal masterpiece of the Italian Renaissance, it has been described as the best known, the most visited, the most written about, the most sung about, and the most parodied work of art in the world. The painting's novel qualities include the subject's enigmatic expression, monumentality of the composition, the subtle modelling of forms, and the atmospheric illusionism.")
 })
 Swing.addEventListener('click', function(){
-    window.open("https://en.wikipedia.org/wiki/The_Swing_(Fragonard)");
+    alert("The Swing also known as The Happy Accidents of the Swing is an 18th-century oil painting by Jean-Honoré Fragonard in the Wallace Collection in London. It is considered to be one of the masterpieces of the Rococo era, and is Fragonard's best-known work. The painting depicts an elegantly dressed young woman on a swing. A smiling young man, hiding in the bushes below and to the left, points towards her billowing dress with hat in hand. A smiling older man, who is nearly hidden in the shadows on the right, propels the swing with a pair of ropes, as a small white dog barks nearby.")
 })
 GirlWithPearl.addEventListener('click', function(){
-    window.open("https://en.wikipedia.org/wiki/Girl_with_a_Pearl_Earring");
+    alert("Girl with a Pearl Earring is an oil painting by Dutch Golden Age painter Johannes Vermeer, dated c. 1665. Going by various names over the centuries, it became known by its present title towards the end of the 20th century because of the earring worn by the girl portrayed there. The work has been in the collection of the Mauritshuis in The Hague since 1902 and has been the subject of various literary and cinematic treatments. The painting is a tronie, the Dutch 17th-century description of a 'head' that was not meant to be a portrait. It depicts a European girl wearing 'exotic dress', an 'oriental turban', and what appears to be a very large pearl as an earring.")
 })
 StarryNight.addEventListener('click', function(){
-    window.open("https://en.wikipedia.org/wiki/The_Starry_Night");
+    alert("The Starry Night is an oil-on-canvas painting by the Dutch Post-Impressionist painter Vincent van Gogh, painted in June 1889. It depicts the view from the east-facing window of his asylum room at Saint-Rémy-de-Provence, just before sunrise, with the addition of an imaginary village. It has been in the permanent collection of the Museum of Modern Art in New York City since 1941, acquired through the Lillie P. Bliss Bequest. Widely regarded as Van Gogh's magnum opus, The Starry Night is one of the most recognizable paintings in Western art.")
 })
 Scream.addEventListener('click', function(){
-    window.open("https://en.wikipedia.org/wiki/The_Scream");
+    alert("The Scream is a composition created by Norwegian artist Edvard Munch in 1893. The agonized face in the painting has become one of the most iconic images in art, seen as symbolizing the anxiety of the human condition. Munch's work, including The Scream, had a formative influence on the Expressionist movement. Munch recalled that he had been out for a walk at sunset when suddenly the setting sun's light turned the clouds 'a blood red'. He sensed an 'infinite scream passing through nature'.")
 })
 GreatWave.addEventListener('click', function(){
-    window.open("https://en.wikipedia.org/wiki/The_Great_Wave_off_Kanagawa");
+    alert("The Great Wave off Kanagawa is a woodblock print by Japanese ukiyo-e artist Hokusai, created in late 1831 during the Edo period of Japanese history. The print depicts three boats moving through a storm-tossed sea, with a large, cresting wave forming a spiral in the centre over the boats and Mount Fuji visible in the background. The print is Hokusai's best-known work and the first in his series Thirty-six Views of Mount Fuji, in which the use of Prussian blue revolutionized Japanese prints. The composition of The Great Wave is a synthesis of traditional Japanese prints and use of graphical perspective developed in Europe, and earned him immediate success in Japan and later in Europe.")
 })
